49729db836e9baa399ae3d0cec57fddabafcc11a
Rein Solis <reinX244@gmail.com>
2025-02-07 15:24:07 +0800

    Initialized db tables and prisma module.

--- nestjs-api-bookmarks/src/auth/auth.service.ts
+++ nestjs-api-bookmarks/src/auth/auth.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+// import { User, Bookmark } from '@prisma/client';
 
 @Injectable({})
 export class AuthService {
+    constructor(private prisma: PrismaService) {}
+
     signup() {
         return { msg: 'Signup' };
     }
