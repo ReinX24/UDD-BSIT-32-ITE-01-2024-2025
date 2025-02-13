18b4fe289e11af94a21a7fc3e33717ccbae82d86
Rein <reinX244@gmail.com>
2025-02-13 23:57:18 +0800

    Finished CRUD section, currently on Data storage section.

--- app-dev/personal-studying/david-gray-react-native/crud-app/app/index.jsx
+++ app-dev/personal-studying/david-gray-react-native/crud-app/app/index.jsx
@@ -11,6 +11,8 @@ import { useState } from "react";
 
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
+import { Inter_500Medium, useFonts } from "@expo-google-fonts/inter";
+
 // Importing the data from a js file
 import { data } from "@/data/todos";
 
@@ -18,6 +20,14 @@ export default function Index() {
     // Reversing the order of the data in our file
     const [todos, setTodos] = useState(data.sort((a, b) => b.id - a.id));
     const [text, setText] = useState("");
+
+    const [loaded, error] = useFonts({
+        Inter_500Medium,
+    });
+
+    if (!loaded && !error) {
+        return null;
+    }
 
     const addTodo = () => {
         // Get the text from the text input
@@ -28,7 +38,7 @@ export default function Index() {
             setTodos([{ id: newId, title: text, completed: false }, ...todos]);
             setText(""); // reset text back to empty when a todo is added
         }
-        console.log(todos);
+        // console.log(todos);
     };
 
     const toggleTodo = (id) => {
@@ -52,7 +62,28 @@ export default function Index() {
     };
 
     const renderItem = ({ item }) => {
-        <View></View>
+        return (
+            <View style={styles.todoItem}>
+                <Text
+                    // Applying multiple styles
+                    style={[
+                        styles.todoText,
+                        item.completed && styles.completedText,
+                    ]}
+                    onPress={() => toggleTodo(item.id)}
+                >
+                    {item.title}
+                </Text>
+                <Pressable onPress={() => removeTodo(item.id)}>
+                    <MaterialCommunityIcons
+                        name="delete-circle"
+                        size={36}
+                        color="red"
+                        selectable={undefined}
+                    />
+                </Pressable>
+            </View>
+        );
     };
 
     return (
@@ -69,7 +100,12 @@ export default function Index() {
                     <Text style={styles.addButtonText}>Add</Text>
                 </Pressable>
             </View>
-            {/* <FlatList data={todos} renderItem={ } keyExtractor={todo => todo.id} contentContainerStyle={{ flexGrow: 1 }} /> */}
+            <FlatList
+                data={todos}
+                renderItem={renderItem}
+                keyExtractor={(todo) => todo.id}
+                contentContainerStyle={{ flexGrow: 1 }}
+            />
         </SafeAreaView>
     );
 }
@@ -78,7 +114,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         // width: "100%",
-        backgroundColor: "black",
+        backgroundColor: "#494D5F",
     },
     inputContainer: {
         flexDirection: "row",
@@ -102,12 +138,34 @@ const styles = StyleSheet.create({
         color: "#EDF7F6",
     },
     addButton: {
-        backgroundColor: "white",
+        backgroundColor: "#EDF7F6",
         borderRadius: 10,
         padding: 10,
     },
     addButtonText: {
         fontSize: 18,
         color: "#494D5F",
+    },
+    todoItem: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        gap: 4,
+        padding: 10,
+        borderBottomColor: "gray",
+        borderBottomWidth: 1,
+        width: "100%",
+        maxWidth: 1024,
+        marginHorizontal: "auto",
+        pointerEvents: "auto",
+    },
+    todoText: {
+        flex: 1,
+        fontSize: 18,
+        color: "#EDF7F6",
+    },
+    completedText: {
+        textDecorationLine: "line-through",
+        color: "gray",
     },
 });
