4ee8afe0068304c8d0eb68569ef298b0eea7e221
Rein <reinX244@gmail.com>
2025-03-12 22:29:15 +0800

    Started redux-toolkit section.

---
+++ app-dev/personal-studying/sangam-react-native/redux-toolkit/App.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const App = (): React.JSX.Element => {
+  return (
+    <View>
+      <Text>React Redux</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
