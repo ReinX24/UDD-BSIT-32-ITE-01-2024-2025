7b70b8f990cd1f94167a69ed78f7db5f4ab7a974
Rein <reinX244@gmail.com>
2025-03-22 22:18:45 +0800

    Started authentication with clerk.

--- app-dev/personal-studying/as-a-programmer-react-native/spotlight-app/app/_layout.tsx
+++ app-dev/personal-studying/as-a-programmer-react-native/spotlight-app/app/_layout.tsx
@@ -1,21 +1,26 @@
+import { COLORS } from "@/constants/theme";
 import { Stack } from "expo-router";
 import { SafeAreaView } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import { ThemeProvider } from "@react-navigation/native";
 
 export default function RootLayout() {
   return (
     // SafeAreaProvider and SafeAreaView makes sure that the content stays
     // on screen
     <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1 }}>
-        <Stack>
-          <Stack.Screen name="index" options={{ title: "Home" }} />
-          <Stack.Screen
-            name="notifications"
-            options={{ title: "Notifications", headerShown: false }}
-          />
+      <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="index" />
+          {/* <Stack.Screen name="old_index" /> */}
         </Stack>
       </SafeAreaView>
+      <StatusBar style="dark" />
     </SafeAreaProvider>
   );
 }
--- app-dev/personal-studying/as-a-programmer-react-native/spotlight-app/app/index.tsx
+++ app-dev/personal-studying/as-a-programmer-react-native/spotlight-app/app/index.tsx
@@ -1,6 +1,18 @@
 import React from "react";
 import { Redirect } from "expo-router";
 
+// import { View, Text } from "react-native";
+// import { styles } from "@/styles/auth.styles";
+// import { Link } from "expo-router";
+
 export default function Index() {
   return <Redirect href="/(tabs)/home" />;
 }
+
+// export default function Index() {
+//   return (
+//     <View style={styles.container}>
+//       <Link href="/old_index">Go To Old Index</Link>
+//     </View>
+//   );
+// }
