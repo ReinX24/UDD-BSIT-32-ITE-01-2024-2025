216098ca5957e1afe9781490e5eccf250d21fb71
Rein <reinX244@gmail.com>
2025-03-26 23:38:34 +0800

    Added temporary stories to home page.

--- app-dev/personal-studying/as-a-programmer-react-native/spotlight-app/app/(tabs)/home.tsx
+++ app-dev/personal-studying/as-a-programmer-react-native/spotlight-app/app/(tabs)/home.tsx
@@ -1,30 +1,55 @@
-import { Image, Pressable, Text, TouchableOpacity, View } from "react-native";
+import {
+  Image,
+  Pressable,
+  Text,
+  TouchableOpacity,
+  View,
+  ScrollView,
+} from "react-native";
 import { styles } from "../../styles/feed.styles";
 import { useAuth } from "@clerk/clerk-expo";
+import { COLORS } from "@/constants/theme";
+import { Ionicons } from "@expo/vector-icons";
+import { STORIES } from "@/constants/mock-data";
+import Story from "@/components/Story";
 
 export default function Home() {
   const { signOut } = useAuth();
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity
-        onPress={() => {
-          signOut();
-        }}
-      >
-        <Text
-          style={{
-            color: "white",
-          }}
-        >
-          Signout
-        </Text>
-      </TouchableOpacity>
-
       {/* Header */}
       <View style={styles.header}>
-          {/* TODO: resume @344 - 1:21 */}
+        <Text style={styles.headerTitle}>spotlight</Text>
+        <TouchableOpacity
+          onPress={() => {
+            signOut();
+          }}
+        >
+          <Ionicons
+            name="log-out-outline"
+            size={24}
+            color={COLORS.white}
+          ></Ionicons>
+        </TouchableOpacity>
       </View>
+      {/* End of Header */}
+
+      <ScrollView
+        horizontal
+        showsVerticalScrollIndicator={false}
+        style={styles.storiesContainer}
+      >
+        {/* Stories */}
+        {STORIES.map((story) => {
+          return <Story key={story.id} story={story}></Story>;
+        })}
+        {/* End of Stories */}
+      </ScrollView>
+
+      {/* Posts */}
+      {/* TODO: continue 344 @5:52 */}
+      {/* End of Posts */}
     </View>
   );
 }
