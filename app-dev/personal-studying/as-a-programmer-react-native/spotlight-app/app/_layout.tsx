27f7c17ec9ce12d449d47b3f982ae7c2d4d21148
Rein <reinX244@gmail.com>
2025-03-29 21:01:27 +0800

    Finished setting font and splash screen image.
    Currently on adding comments to posts.

--- app-dev/personal-studying/as-a-programmer-react-native/spotlight-app/app/_layout.tsx
+++ app-dev/personal-studying/as-a-programmer-react-native/spotlight-app/app/_layout.tsx
@@ -4,8 +4,25 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import InitialLayout from "@/components/InitialLayout";
 import ClerkAndConvexProdivder from "@/providers/ClerkAndConvexProdivder";
+import { SplashScreen } from "expo-router";
+import { useFonts } from "expo-font";
+import { useCallback } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  // Loading custom fonts
+  const [fontsLoaded] = useFonts({
+    "JetBrainsMono-Medium": require("../assets/fonts/JetBrainsMono-Medium.ttf"),
+  });
+
+  // Once our fonts are loaded, we will hide the splash screen
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      return await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
   return (
     // SafeAreaProvider and SafeAreaView makes sure that the content stays
     // on screen
@@ -16,6 +33,7 @@ export default function RootLayout() {
             flex: 1,
             backgroundColor: "black",
           }}
+          onLayout={onLayoutRootView}
         >
           <InitialLayout />
           {/* <Stack
---
+++ app-dev/personal-studying/as-a-programmer-react-native/spotlight-app/convex/comments.ts
@@ -0,0 +1,15 @@
+import { v } from "convex/values";
+import { mutation } from "./_generated/server";
+import { getAuthenticatedUser } from "./users";
+
+export const addComment = mutation({
+  args: {
+    content: v.string(),
+    postId: v.id("posts"),
+  },
+  handler: async (ctx, args) => {
+    const currentUser = await getAuthenticatedUser(ctx);
+
+    const post = await ctx.db.get(args.postId);
+  },
+});
