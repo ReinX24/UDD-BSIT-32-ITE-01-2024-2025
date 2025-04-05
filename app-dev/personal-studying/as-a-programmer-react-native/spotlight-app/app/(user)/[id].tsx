93f40c7125c4bbc44e7c7333c2f47dad615d9a36
Rein <reinX244@gmail.com>
2025-04-05 17:06:30 +0800

    Adding functionality to view other users' profiles.

---
+++ app-dev/personal-studying/as-a-programmer-react-native/spotlight-app/app/(user)/[id].tsx
@@ -0,0 +1,14 @@
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react";
+import { View, Text } from "react-native";
+
+// For viewing profiles of other users
+export default function UserProfileScreen() {
+  // const profile = useQuery(api.users.getUserProfile, );
+
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+}
--- app-dev/personal-studying/as-a-programmer-react-native/spotlight-app/convex/users.ts
+++ app-dev/personal-studying/as-a-programmer-react-native/spotlight-app/convex/users.ts
@@ -1,5 +1,6 @@
-import { mutation, MutationCtx, query, QueryCtx } from "./_generated/server";
 import { v } from "convex/values";
+import { Id } from "./_generated/dataModel";
+import { mutation, MutationCtx, query, QueryCtx } from "./_generated/server";
 
 // Create a new user
 export const createUser = mutation({
@@ -88,4 +89,98 @@ export async function getAuthenticatedUser(ctx: QueryCtx | MutationCtx) {
   }
 
   return currentUser;
+}
+
+export const getUserProfile = query({
+  args: {
+    id: v.id("users"),
+  },
+  handler: async (ctx, args) => {
+    const user = await ctx.db.get(args.id);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    return user;
+  },
+});
+
+// Checks if the current user is following the user
+export const isFollowing = query({
+  args: { followingId: v.id("users") },
+  handler: async (ctx, args) => {
+    const currentUser = await getAuthenticatedUser(ctx);
+
+    const follow = await ctx.db
+      .query("follows")
+      .withIndex("by_both", (q) => {
+        return q
+          .eq("followerId", currentUser._id)
+          .eq("followingId", args.followingId);
+      })
+      .first();
+
+    return !!follow; // returns true or false
+  },
+});
+
+export const toggleFollow = mutation({
+  args: { followingId: v.id("users") },
+  handler: async (ctx, args) => {
+    const currentUser = await getAuthenticatedUser(ctx);
+
+    // Checking if the current user follows the other user
+    const existing = await ctx.db
+      .query("follows")
+      .withIndex("by_both", (q) => {
+        return q
+          .eq("followerId", currentUser._id)
+          .eq("followingId", args.followingId);
+      })
+      .first();
+
+    if (existing) {
+      // Unfollow
+      await ctx.db.delete(existing._id);
+      await updateFollowCounts(ctx, currentUser._id, args.followingId, false);
+    } else {
+      // Follow
+      await ctx.db.insert("follows", {
+        followerId: currentUser._id,
+        followingId: args.followingId,
+      });
+      await updateFollowCounts(ctx, currentUser._id, args.followingId, true);
+
+      // Create a notification
+      await ctx.db.insert("notifications", {
+        receiverId: args.followingId,
+        senderId: currentUser._id,
+        type: "follow",
+      });
+    }
+  },
+});
+
+// Helper function for toggleFollow
+async function updateFollowCounts(
+  ctx: MutationCtx,
+  followerId: Id<"users">,
+  followingId: Id<"users">,
+  isFollow: boolean
+) {
+  const follower = await ctx.db.get(followerId);
+  const following = await ctx.db.get(followingId);
+
+  if (follower && following) {
+    // Increments or decrements the following count of current user
+    await ctx.db.patch(followerId, {
+      following: follower.following + (isFollow ? 1 : -1),
+    });
+
+    // Increments or decrements the follower count of the followed user
+    await ctx.db.patch(followingId, {
+      followers: following.following + (isFollow ? 1 : -1),
+    });
+  }
 }
