1b00ead7d6711789d9c4d0ca2ff2d666312596bc
Rein <reinX244@gmail.com>
2025-04-25 23:23:46 +0800

    Finished profile header for UserProfile.

--- app-dev/personal-studying/threads-clone/components/UserProfile.tsx
+++ app-dev/personal-studying/threads-clone/components/UserProfile.tsx
@@ -1,8 +1,9 @@
 import { COLORS } from "@/constants/COLORS";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
+import { useUserProfile } from "@/hooks/useUserProfile";
 import { useQuery } from "convex/react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 type UserProfileProps = {
   userId?: string;
@@ -12,7 +13,10 @@ const UserProfile = ({ userId }: UserProfileProps) => {
   const profile = useQuery(api.users.getUserById, {
     userId: userId as Id<"users">,
   });
-  console.log("UserProfile - profile:", profile);
+  const { userProfile } = useUserProfile();
+  const isSelf = userId === userProfile?._id;
+
+  // console.log("UserProfile - profile:", profile);
 
   return (
     <View style={styles.container}>
@@ -24,6 +28,36 @@ const UserProfile = ({ userId }: UserProfileProps) => {
           <Text style={styles.username}>@{profile?.username}</Text>
         </View>
         <Image source={{ uri: profile?.imageUrl }} style={styles.image} />
+      </View>
+      <Text style={styles.bio}>{profile?.bio || "No bio"}</Text>
+      <Text>
+        {profile?.followersCount} • {profile?.websiteUrl || "No website"}
+      </Text>
+
+      <View style={styles.buttonRow}>
+        {/* Shows if we are viewing our own profile */}
+        {isSelf && (
+          <>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Edit Profile</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Share Profile</Text>
+            </TouchableOpacity>
+          </>
+        )}
+
+        {/* Shows if we are viewing the profile of another user */}
+        {!isSelf && (
+          <>
+            <TouchableOpacity style={styles.fullButton}>
+              <Text style={styles.fullButtonText}>Follow</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.fullButton}>
+              <Text style={styles.fullButtonText}>Mention</Text>
+            </TouchableOpacity>
+          </>
+        )}
       </View>
     </View>
   );
@@ -57,5 +91,41 @@ const styles = StyleSheet.create({
     width: 50,
     height: 50,
     borderRadius: 25,
+  },
+  bio: {
+    marginTop: 16,
+    marginBottom: 16,
+    fontSize: 14,
+  },
+  buttonRow: {
+    flexDirection: "row",
+    gap: 16,
+    justifyContent: "space-evenly",
+    marginTop: 16,
+  },
+  button: {
+    flex: 1,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: COLORS.border,
+    borderRadius: 4,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    fontWeight: "bold",
+  },
+  fullButton: {
+    flex: 1,
+    padding: 10,
+    borderWidth: 1,
+    backgroundColor: "#000",
+    borderRadius: 4,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  fullButtonText: {
+    fontWeight: "bold",
+    color: COLORS.background,
   },
 });
